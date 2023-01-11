da2052512966aabae88467e5b25f3adfa5882432
ma867 <ma867@njit.edu>
2023-01-11 17:47:59 -0500

    last changes

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -2,11 +2,13 @@
 
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import { useState } from "react"
 
 export default function AuthPage(props){
+
     return(
         <main>
-            <h1>Auth Page</h1>
+
             <SignUpForm setUser={props.setUser}/>
             <LoginForm setUser={props.setUser}/>
         </main>
